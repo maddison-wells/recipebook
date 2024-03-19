f4d96200440a63aad83b683f058076965b769d03
Maddison Wells <maddisonwells@Maddisons-MacBook-Pro.local>
2024-03-19 16:49:16 +1100

    started backend

---
+++ backend/recipebook/src/test/java/io/recipe/recipebook/RecipebookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.recipe.recipebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
